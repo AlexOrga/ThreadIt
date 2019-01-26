cf503224da6d8b5d9ddf84681795a27bca268e71
Alex Orga <alexorga.dev@gmail.com>
2019-01-26 09:59:05 -0600

    added model for OrderQuoteProductPair

---
+++ ThreadIt/Models/OrderQuoteProductPair.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThreadIt.Models
+{
+    public class OrderQuoteProductPair
+    {
+        public int id { get; set; }
+        public int? order_id { get; set; }
+        public int? quote_id { get; set; }
+        public int product_id { get; set; }
+        public int quantity { get; set; }
+        public double cost_per_item { get; set; }
+        public double price_per_item { get; set; }
+        public bool is_order { get; set; }
+}
+}
