b16dfc11168d4c4ae5ca7b7e3f66f04253d30763
Alex Orga <alexorga.dev@gmail.com>
2019-01-26 09:46:09 -0600

    added model for clientEmployeeNamePair

---
+++ ThreadIt/Models/ClientEmployeeNamePair.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThreadIt.Models
+{
+    public class ClientEmployeeNamePair
+    {
+        public int id { get; set; }
+        public int client_id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string telephone_number { get; set; }
+        public string comments { get; set; }
+    }
+}
