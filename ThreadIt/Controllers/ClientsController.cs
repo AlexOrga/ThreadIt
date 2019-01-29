1ec62a26ec636be39bec6ca9f0445fdf9cd4a1ee
Alex Orga <alexorga.dev@gmail.com>
2019-01-28 18:23:44 -0600

    able to retrieve all clients and a single client by their id

---
+++ ThreadIt/Controllers/ClientsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using ThreadIt.DataAccess;
+using ThreadIt.Models;
+
+namespace ThreadIt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly ClientStorage _storage;
+
+        public ClientsController(IConfiguration config)
+        {
+            _storage = new ClientStorage(config);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllClients()
+        {
+            return Ok(_storage.GetAllClients());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingleClient(int id)
+        {
+            return Ok(_storage.GetSingleClient(id));
+        }
+    }
+}
---
+++ ThreadIt/DataAccess/ClientStorage.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using ThreadIt.Models;
+using System.Data.SqlClient;
+
+namespace ThreadIt.DataAccess
+{
+    public class ClientStorage
+    {
+        public readonly string conString;
+
+        public ClientStorage(IConfiguration config)
+        {
+            conString = config.GetSection("ConnectionString").Value;
+        }
+
+        public IEnumerable<Client> GetAllClients()
+        {
+            using (var connection = new SqlConnection(conString))
+            {
+                connection.Open();
+
+                var result = connection.Query<Client>(@"SELECT *
+                                                        FROM clients
+                                                        ORDER BY clients.name");
+
+                return result;
+            }
+        }
+
+        public IEnumerable<Client> GetSingleClient(int id)
+        {
+            using (var connection = new SqlConnection(conString))
+            {
+                connection.Open();
+
+                var result = connection.Query<Client>(@"SELECT *
+                                                        FROM clients
+                                                        WHERE clients.id = @id", new { id });
+
+                return result;
+            }
+        }
+    }
+}
